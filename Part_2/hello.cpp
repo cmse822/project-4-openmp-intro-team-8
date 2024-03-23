57b94f4da5f3567972d55ea11715492d677febe6
Berk Atik <berkatik@Berks-MBP.attlocal.net>
2024-03-22 22:39:48 -0400

    feat: updated part 2 code for mpi thread support

--- Part_2/hello.cpp
+++ Part_2/hello.cpp
@@ -1,31 +1,25 @@
 #include "mpi.h"
 #include <omp.h>
-//#include <iostream>
 #include <stdio.h>
-//using namespace std;
 
 int main(int argc, char *argv[]) 
 {
     int numtasks, rank;
     //printf("Hello, World! (Before Init)\n");
-    MPI_Init(&argc, &argv);
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    //printf("Hello, World! (After Init, Before Finalize)\n");
+
     int name_length = MPI_MAX_PROCESSOR_NAME;
     char proc_name[name_length];
     MPI_Get_processor_name(proc_name, &name_length);
 
     #pragma omp parallel
     {
-        int tid = omp_get_thread_num();
-        printf("Hello, World! (from thread %d of %d)\n", tid, omp_get_num_threads());
-        printf("Process %d%d is running on node <<%s>>\n", procid, nprocs, proc_name);
+        int thread_id = omp_get_thread_num();
+        printf("MPI Rank: %d, Thread: %d, on node: %s\n", rank, thread_id, proc_name);
     }
 
-
-
     MPI_Finalize();
-    //printf("Hello, World! (After Finalize)\n");
     return 0;
 }
