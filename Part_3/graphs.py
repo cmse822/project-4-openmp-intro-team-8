55968b61b69fcbc1a3671ce06931865f8a875590
Berk Atik <berkatik@Berks-MBP.attlocal.net>
2024-03-26 20:44:44 -0400

    feat: added graphs for part 3

--- Part_3/graphs.py
+++ Part_3/graphs.py
@@ -1,61 +1,72 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import seaborn as sns
 
 # Load the data
-data = pd.read_csv('data.csv')  # Make sure to use the correct file name
+data = pd.read_csv('performance_data.csv')
 
-# Plotting time to solution vs. matrix size for different configurations
-def plot_time_vs_matrix_size(data, mpi_ranks, threads_per_rank):
+def plot_time_vs_mpi_ranks_by_matrix_size(data):
     '''
-        Plot time to solution vs. matrix size for different configurations of MPI ranks and threads per rank
-        
-        Parameters:
-            data (DataFrame): The performance data
-            mpi_ranks (int): The number of MPI ranks
-            threads_per_rank (int): The number of threads per rank
-            
-        Returns:
-            None
+    Plot time to solution vs. MPI ranks for each matrix size in separate subplots.
     '''
-    filtered_data = data[(data['MPI_Ranks'] == mpi_ranks) & (data['Threads_Per_Rank'] == threads_per_rank)]
-    plt.figure(figsize=(10, 6))
-    for size in filtered_data['Matrix_Size'].unique():
-        subset = filtered_data[filtered_data['Matrix_Size'] == size]
-        plt.plot(subset['Matrix_Size'], subset['Time'], marker='o', label=f'Size {size}')
+    matrix_sizes = data['Matrix_Size'].unique()
+    fig, axes = plt.subplots(nrows=len(matrix_sizes), ncols=1, figsize=(12, 8), sharex=True, sharey=True)
+    
+    for ax, matrix_size in zip(axes, matrix_sizes):
+        for threads_per_rank in data['Threads_Per_Rank'].unique():
+            subset = data[(data['Matrix_Size'] == matrix_size) & (data['Threads_Per_Rank'] == threads_per_rank)]
+            ax.plot(subset['MPI_Ranks'], subset['Time'], marker='o', linestyle='-', label=f'Threads {threads_per_rank}')
+        ax.set_title(f'Matrix Size: {matrix_size}')
+        ax.grid(True)
+        ax.set_xlabel('MPI Ranks')
+        ax.set_ylabel('Time to Solution (s)')
+        if ax == axes[-1]:
+            ax.legend()
 
-    plt.xlabel('Matrix Size')
-    plt.ylabel('Time to Solution (s)')
-    plt.title(f'Time to Solution vs. Matrix Size (MPI Ranks: {mpi_ranks}, Threads/Rank: {threads_per_rank})')
-    plt.legend()
-    plt.grid(True)
+    plt.tight_layout()
     plt.show()
 
-# Example usage for MPI Ranks = 2, Threads/Rank = 4
-
-# Plotting time to solution for different MPI ranks and threads per rank for a fixed matrix size
-def plot_time_vs_configurations(data, matrix_size):
+def plot_time_vs_matrix_size_by_mpi_rank(data):
     '''
-        Plot a heatmap of time to solution for different configurations of MPI ranks and threads per rank
-
-        Parameters:
-            data (DataFrame): The performance data
-            matrix_size (int): The matrix size to filter the data on
-        
-        Returns:
-            None
+    Plot time to solution vs. matrix size for each MPI rank in separate subplots.
     '''
-    filtered_data = data[data['Matrix_Size'] == matrix_size]
-    pivot_table = filtered_data.pivot("MPI_Ranks", "Threads_Per_Rank", "Time")
+    mpi_ranks = data['MPI_Ranks'].unique()
+    fig, axes = plt.subplots(nrows=len(mpi_ranks), ncols=1, figsize=(12, 8), sharex=True, sharey=True)
+    
+    for ax, mpi_rank in zip(axes, mpi_ranks):
+        for threads_per_rank in data['Threads_Per_Rank'].unique():
+            subset = data[(data['MPI_Ranks'] == mpi_rank) & (data['Threads_Per_Rank'] == threads_per_rank)]
+            ax.plot(subset['Matrix_Size'], subset['Time'], marker='o', linestyle='-', label=f'Threads {threads_per_rank}')
+        ax.set_title(f'MPI Ranks: {mpi_rank}')
+        ax.grid(True)
+        ax.set_xlabel('Matrix Size')
+        ax.set_ylabel('Time to Solution (s)')
+        if ax == axes[-1]:
+            ax.legend()
 
+    plt.tight_layout()
+    plt.show()
+    
+def plot_heatmap_for_matrix_size(data, matrix_size):
+    '''
+    Plot a heatmap showing time to solution for different configurations of MPI ranks and threads per rank for a given matrix size.
+    '''
     plt.figure(figsize=(10, 6))
+    subset = data[data['Matrix_Size'] == matrix_size]
+    pivot_table = subset.pivot("MPI_Ranks", "Threads_Per_Rank", "Time")
     sns.heatmap(pivot_table, annot=True, fmt=".2f", cmap="YlGnBu")
     plt.xlabel('Threads Per Rank')
     plt.ylabel('MPI Ranks')
     plt.title(f'Time to Solution Heatmap (Matrix Size: {matrix_size})')
     plt.show()
 
-
 if __name__ == '__main__':
-    plot_time_vs_matrix_size(data, mpi_ranks=2, threads_per_rank=4)
-    plot_time_vs_configurations(data, matrix_size=)
+    plot_time_vs_mpi_ranks_by_matrix_size(data)
+    plot_time_vs_matrix_size_by_mpi_rank(data)
+
+
+    matrix_sizes = [20, 100, 500]
+    for matrix_size in matrix_sizes:
+        plot_heatmap_for_matrix_size(data, matrix_size)
+        
+
